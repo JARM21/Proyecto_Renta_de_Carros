5e1ff4e5e1b034c6d2d79d06fb6571f3ec5c2db4
Davi1705 <d.gomeza1@fmsnor.org>
2021-11-13 12:26:57 -0500

    creación repositorios lb4
    Signed-off-by: Davi1705 <d.gomeza1@fmsnor.org>

---
+++ backendrentacarros/src/repositories/index.ts
@@ -0,0 +1,6 @@
+export * from './administrador.repository';
+export * from './alquiler.repository';
+export * from './asesor.repository';
+export * from './cliente.repository';
+export * from './solicitud.repository';
+export * from './vehiculo.repository';
